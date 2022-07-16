bedb4f81e3397fe8c67a7aa34d9388912026ace9
Larysa Zaremba <larysa.zaremba@gmail.com>
2022-07-16 20:11:08 +0200

    use SDL

--- main.c
+++ main.c
@@ -1,6 +1,51 @@
-#include <stdio.h>
+#include <SDL.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+#include <stdlib.h>
 
-int main() {
-    printf("hello, world!\n");
-    return 0;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
+
+void drawRandomPixels()
+{
+    if (SDL_MUSTLOCK(surface))
+        SDL_LockSurface(surface);
+
+    Uint8 *pixels = surface->pixels;
+
+    for (int i = 0; i < 1048576; i++)
+    {
+        char randomByte = rand() % 255;
+        pixels[i] = randomByte;
+    }
+
+    if (SDL_MUSTLOCK(surface))
+        SDL_UnlockSurface(surface);
+
+    SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(screenTexture);
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
+    surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(drawRandomPixels, 0, 1);
+#else
+    while (1)
+    {
+        drawRandomPixels();
+        SDL_Delay(16);
+    }
+#endif
 }
